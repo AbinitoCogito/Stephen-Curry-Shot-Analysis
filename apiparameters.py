20f7c14322173183b091717b3eebeed24821f7b3
Ahmet Ekrem Yıldız <81154496+AbinitoCogito@users.noreply.github.com>
2024-10-16 10:11:51 +0300

    Create apiparameters.py

---
+++ apiparameters.py
@@ -0,0 +1,14 @@
+"""
+API parameters that had been used
+"""
+.ShotChartDetail(
+    team_id=0,  
+    player_id='201939',
+    context_measure_simple='FGA',
+    season_type_all_star=['Regular Season', 'Playoffs'],
+    season_nullable=['2014-15', '2015-16', '2016-17', '2017-18', '2018-19',
+                     '2019-20', '2020-21', '2021-22', '2022-23', '2023-24'] 
+)
+.PlayerCareerStats(player_id="201939")
+.get_players()
+.get_teams()
